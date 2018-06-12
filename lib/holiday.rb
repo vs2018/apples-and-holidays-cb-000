da6baf31363144a4bd758913fc6e4240ebc99d8d
Vishal Sharma <vsh1819@gmail.com>
2018-06-12 10:32:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -43,6 +43,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_supplies.each do |key, value|
     key[holiday_name] = supply_array
   end
+  
+  puts holiday_supplies
 
 end
 
