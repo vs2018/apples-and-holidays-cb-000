81825294daf2cc592c5198f1e29532ee509fca49
Vishal Sharma <vsh1819@gmail.com>
2018-06-12 10:54:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,6 +71,9 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_supplies.each do |key, value|
     puts "#{key.to_s.capitalize!}:"
+    value.each do |holiday, data|
+      puts "#{key.to_s.capitalize!}""
+    end
   end
 
 end
