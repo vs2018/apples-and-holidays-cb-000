2ce940af75c163fdf2557feb7045920809592d42
Vishal Sharma <vsh1819@gmail.com>
2018-06-12 10:51:01 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,6 +68,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
+  holiday_supplies.each do |key, value|
+    puts key.to_s
+  end
 
 end
 
