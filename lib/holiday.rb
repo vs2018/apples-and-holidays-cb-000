1ac4f634a850993b7062d2cc395cd37cad93d8f7
Vishal Sharma <vsh1819@gmail.com>
2018-06-12 10:28:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -32,8 +32,7 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
+  
 
 end
 
